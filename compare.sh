e0373e752ec99b43e6287d594645d2a6ba260557
Ben Chatelain <ben@octop.ad>
2020-03-01 17:50:59 -0700

    ✨ Don't require Jenkins formula to be installed for comparison

--- compare.sh
+++ compare.sh
@@ -1,8 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-brew_home=/home/linuxbrew/.linuxbrew
-formula_path=$brew_home/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/
+# brew_home=/home/linuxbrew/.linuxbrew
+# jenkins_formula_path=$brew_home/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/jenkins.rb
 
-/usr/bin/diff \
-    Formula/pbjenkins.rb \
-    $formula_path/jenkins.rb
+jenkins_formula_path=~/tmp/jenkins_formula.rb
+local_formula_path=Formula/pbjenkins.rb
+
+# Write a copy of the Jenkins Homebrew formula to a temp folder.
+# This way the formula doesn't need to be installed.
+brew cat jenkins > $jenkins_formula_path
+
+command diff \
+    $local_formula_path \
+    $jenkins_formula_path \
+    | grep -B 1 "sha256"
