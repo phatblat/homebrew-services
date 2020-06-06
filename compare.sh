244b4067148804ea7913a1665aa7606baef88149
Ben Chatelain <ben@octop.ad>
2020-06-06 14:53:39 -0600

    📜 Write temp formula for comparison when not installed

--- compare.sh
+++ compare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
 # brew_home=/home/linuxbrew/.linuxbrew
-# jenkins_formula_path=$brew_home/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/jenkins.rb
 
-jenkins_formula_path=~/tmp/jenkins_formula.rb
+jenkins_formula_path=(brew_home)/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/jenkins.rb
+if ! test -f "$jenkins_formula_path"; then
+    # When the jenkins formula is not installed, write a tmp copy for comparison.
+    jenkins_formula_path=/tmp/jenkins_formula.rb
+    brew cat jenkins > $jenkins_formula_path
+fi
 local_formula_path=Formula/pbjenkins.rb
 
 # Write a copy of the Jenkins Homebrew formula to a temp folder.
